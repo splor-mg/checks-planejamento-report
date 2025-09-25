d8762e20d4c6c79e789b4a9e6fadba5e2b61a6be
Gabriel Dornas <gabrielbdornas@gmail.com>
2025-09-25 14:47:27 -0300

    Improve scripts.R to use writexl
    Not importantion or install needed
    See #9

--- scripts/check.R
+++ scripts/check.R
@@ -1,7 +1,7 @@
 Sys.setenv(LOG_FILE = "checks-planejamento.jsonl")
 
 library(data.table)
-library(openxlsx)
+library(writexl)
 
 sigplan <- dpm::read_datapackage("datapackages/sigplan/datapackage.json")
 sisor <- dpm::read_datapackage("datapackages/sisor/datapackage.json")
@@ -32,17 +32,15 @@ checks <- checksplanejamento::check_all(
 
 logs <- jsonlite::stream_in(file("checks-planejamento.jsonl")) |> data.table::as.data.table()
 stopifnot(nrow(checks[valid == FALSE]) == length(unique(logs$type)))
-# data.table::fwrite(logs[, .(type, message)], "data-results/checks-planejamento.csv", bom = TRUE, dec = ",", sep = ";")
 
 error_summary <- logs[valid == FALSE, .N, by = type]
 setnames(error_summary, old = "N", new = "Número de Erros")
-# data.table::fwrite(error_summary, "data-results/checks-planejamento-resumo.csv", bom = TRUE, dec = ",", sep = ";")
-write.xlsx(logs[, .(type, message)],
-           file = "data-results/checks-planejamento.xlsx",
-           overwrite = TRUE)
 
-logs <- jsonlite::stream_in(file("checks-planejamento.jsonl")) |> data.table::as.data.table()
-stopifnot(nrow(checks[valid == FALSE]) == length(unique(logs$type)))
-write.xlsx(error_summary,
-           file = "data-results/checks-planejamento-resumo.xlsx",
-           overwrite = TRUE)
+# Save logs and summary as Excel using writexl
+write_xlsx(
+  list(
+    logs = logs[, .(type, message)],
+    resumo = error_summary
+  ),
+  path = "data-results/checks-planejamento.xlsx"
+)
